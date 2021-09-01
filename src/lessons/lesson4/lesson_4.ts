7d07db095e81e53914e0799ea1ecf189bafd9daa
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-09-01 16:46:47 +0300

    lesson 5 completed

--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -1,25 +1,34 @@
-console.log('lesson 4');
+console.log('lesson 4')
 
 // http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
 // https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/
-
 
 // Task 01
 // Создайте промис, который постоянно находиться в состоянии pending.
 // В конструкторе промиса выведите в консоль сообщение "Promise is created".
 
+const promise1 = new Promise((resolve, reject) => {
+  //console.log('Promise is created')
+})
+//console.log(promise1)
 
 // Task 02
 // Создайте промис, который после создания сразу же переходит в состояние resolve
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
 
+const promise2 = new Promise((resolve, reject) => {
+  resolve('Promise Data')
+}).then(console.log)
 
 // Task 03
 // Создайте промис, который после создания сразу же переходит в состояние rejected
 // и возвращает строку 'Promise Error'
 // Получите данные промиса и выведите их в консоль
 
+const promise3 = new Promise((resolve, reject) => {
+  reject('Promise Error')
+}).catch(console.log)
 
 // Task 04
 // Создайте промис, который переходит в состояние resolved через 3с.
@@ -27,6 +36,9 @@ console.log('lesson 4');
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
 
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('Promise Data 4'), 3000)
+}).then(console.log)
 
 // Task 05
 // Создайте литерал объекта handlePromise со следующими свойствами:
@@ -37,10 +49,34 @@ console.log('lesson 4');
 // вторая - `Promise is rejected with error: ${paramName}`
 // Создайте три обработчика события click для кнопок "Create Promise", "Resolve Promise", "Reject Promise".
 // Первый обработчик, создает промис, заполняет первые три свойства,
-// описаного выше объекта: свойство promise получает новый созданный промис,
+// описанного выше объекта: свойство promise получает новый созданный промис,
 // свойства resolve и reject получают ссылки на соответствующие функции
 // resolve и reject. Следующие два обработчика запускают методы resolve и reject.
 
+const handlePromise = {
+  promise: null,
+  resolve: null,
+  reject: null,
+  onSuccess: (paramName: string) => {
+    console.log(`Promise is resolved with data: ${paramName}`)
+  },
+  onError: (paramName: string) => {
+    console.log(`Promise is rejected with error: ${paramName}`)
+  },
+}
+
+const handleClickToCreatePromise = () => {
+  // let res
+  // let rej
+  const promise = new Promise((resolve, reject) => {
+    //@ts-ignore
+    handlePromise.resolve = resolve
+    //@ts-ignore
+    handlePromise.reject = reject
+  })
+}
+const handleClickToResolvePromise = () => {}
+const handleClickToRejectPromise = () => {}
 
 // Task 06
 // Создайте промис, который через 1 с возвращает строку "My name is".
@@ -49,6 +85,15 @@ console.log('lesson 4');
 // Создайте функцию print, которая выводит в консоль значение своего параметра
 // Добавьте два метода then и передайте созданные функции.
 
+const onSuccess = (string: any) => `${string}Yan`
+const print = (string: string) => console.log(string)
+
+const prom6: PromiseLike<string | void> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(`My name is `)
+  }, 2000)
+})
+prom6.then(onSuccess).then(print)
 
 // Task 7
 // Создайте три промиса. Первый промис возвращает объект { name: "Anna" } через 2с,
@@ -56,7 +101,28 @@ console.log('lesson 4');
 // Получите результаты работы промисов, объедините свойства объектов
 // и выведите в консоль {name, age, city}
 
+const prom7_1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({ name: 'Yan' })
+  }, 2000)
+})
+const prom7_2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({ age: 33 })
+  }, 3000)
+})
+const prom7_3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({ city: 'Bobruisk Megacity' })
+  }, 4000)
+})
 
+Promise.allSettled([prom7_1, prom7_2, prom7_3]).then((result: any) => {
+  const combinedProperties = result.reduce((acc: any, elem: any) => {
+    return { ...acc, ...elem.value }
+  }, {})
+  console.log(combinedProperties)
+})
 
 // just a plug
-export default ()=>{};
+export default () => {}
--- src/lessons/lesson5/lesson5.ts
+++ src/lessons/lesson5/lesson5.ts
@@ -1,4 +1,4 @@
-console.log('Lesson 5');
+console.log('Lesson 5')
 
 // Keyword - this
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/this
@@ -20,20 +20,25 @@ console.log('Lesson 5');
 // https://learn.javascript.ru/call-apply-decorators
 // https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%BE-%D0%BE-%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D0%B0%D1%85-apply-call-%D0%B8-bind-%D0%BD%D0%B5%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%B8%D0%BC%D1%8B%D1%85-%D0%BA%D0%B0%D0%B6%D0%B4%D0%BE%D0%BC%D1%83-javascript-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D1%83-ddd5f9b06290
 
-
 // Task 01
 // Дан объект someObj, реализуйте функцию greeting и присвойте ее ключу объекта с аналогичным именем.
 // Функция должна вернуть строку `My name is ${name}. I am ${age}`, где name и age берутся из свойств объекта
 
 type someObjType = {
-    name: string;
-    age: number;
+  name: string
+  age: number
+  greeting: () => string
 }
 
-let someObj:someObjType = {
-    name: 'Eugene',
-    age: 32
+let someObj: someObjType = {
+  name: 'Eugene',
+  age: 32,
+  greeting: function () {
+    return `My name is ${this.name}. I am ${this.age}`
+  },
 }
+
+console.log(someObj.greeting())
 
 // Task 02
 // реализовать счетчик counter в виде объекта со следующими методами:
@@ -44,18 +49,88 @@ let someObj:someObjType = {
 // rest current count - устанавливает значение счетчика равным 0
 // все методы должны ссылаться на сам объект
 
+const counter = {
+  counter: 0,
+  getCurrentCount: function () {
+    return this.counter
+  },
+  increment: function () {
+    ++this.counter
+  },
+  decrement: function () {
+    --this.counter
+  },
+  setCurrentCount: function (num: number) {
+    this.counter = num
+  },
+  restCurentCount: function (num: number) {
+    this.counter = 0
+  },
+}
+
 // Task 03
 // переделайте код из Task 02, что бы сработал следующий код:
 // counter.setCurrentCount(10).increment().increment().increment().decrement().getCurrentCount() // 12
+
+const counter2 = {
+  counter: 0,
+  getCurrentCount: function () {
+    return this.counter
+  },
+  increment: function () {
+    ++this.counter
+    return this
+  },
+  decrement: function () {
+    --this.counter
+    return this
+  },
+  setCurrentCount: function (num: number) {
+    this.counter = num
+    return this
+  },
+  restCurentCount: function (num: number) {
+    this.counter = 0
+  },
+}
+
+console.log(
+  counter2
+    .setCurrentCount(10)
+    .increment()
+    .increment()
+    .increment()
+    .decrement()
+    .getCurrentCount()
+)
 
 // Task 04
 // Написать функцию конструктор myFirstConstructorFunc которая принимает 2 параметра name и age и возвращает объект
 // у которого будут эти свойства и метод greeting из Task 01
 
+function myFirstConstructorFunc(name: string, age: number): any {
+  //@ts-ignore
+  this.name = name
+  //@ts-ignore
+  this.age = age
+  //@ts-ignore
+  this.greeting = someObj.greeting
+}
+//@ts-ignore
+const a = new myFirstConstructorFunc('Ivan', 20)
+console.log(a.greeting())
+
 // Task 05 есть 2 объекта One и Two. С помощью bind и метода sayHello заставьте поздороваться объект One
 
-let One = {name: 'One'};
-let Two = {name: 'Two', sayHello: function() {console.log(`Hello, my name is ${this.name}`)}};
+let One = { name: 'One' }
+let Two = {
+  name: 'Two',
+  sayHello: function () {
+    console.log(`Hello, my name is ${this.name}`)
+  },
+}
+
+//Two.sayHello.bind(One)()
 
 // Task 06
 // создайте объект helperObj у которого есть следующие методы:
@@ -64,19 +139,85 @@ let Two = {name: 'Two', sayHello: function() {console.log(`Hello, my name is ${t
 // greeting - используется функция sayHello из Task 05
 // можно использовать @ts-ignore
 
+const helperObj = {
+  name: 'Igor111' + '',
+  age: 22,
+  //@ts-ignore
+  changeName: function (newName: string) {
+    //@ts-ignore
+    this.name = newName
+  },
+  //@ts-ignore
+  setAge: function (newAge: number) {
+    //@ts-ignore
+    this.age = newAge
+  },
+  greeting: Two.sayHello,
+}
+
+helperObj.greeting()
+
 // Bind
 // 1) Дана функция sumTwoNumbers, реализовать функцию bindNumber которая принимает функцию sumTwoNumbers и число, и
 // возвращает другую функцию, которое также принимает число и возвращает сумму этих чисел. Замыкание использовать нельзя
-function sumTwoNumbers(a:number,b:number):number {return a + b};
+function sumTwoNumbers(a: number, b: number): number {
+  return a + b
+}
+
+function bindNumber(func: any, num: number) {
+  return function (num2: number) {
+    return num + num2
+  }
+}
+
+console.log(bindNumber(sumTwoNumbers, 4)(5))
 
 // 2) Напишите функцию которая принимает первым аргументом объект One, а вторым helperObj. Данная функция
 // возвращает другую функцию которая принимает строку в качестве аргумента и устанавливает ее свойству name объекта One
+
+function func(obj: any, f: any) {
+  return (str: string) => f.changeName.bind(obj)(str)
+}
+
+func(One, helperObj)('Bind2')
+console.log(One.name)
+
 // 3) Одной строкой установить с помощью helperObj объекту Two поле age в значение 30
+
+helperObj.setAge.bind(Two)(30)
+//@ts-ignore
+console.log(Two.age)
+
 // 4) Создать метод hi у объекта One, который всегда вызывает метод greeting объекта helperObj от имени Two
+//@ts-ignore
+One.hi = function () {
+  helperObj.greeting.bind(Two)()
+}
+//@ts-ignore
+One.hi()
 
 // Реализовать задачи 2-4 из Bind с помощью Call
 
+//2
+function funcCall(obj: any, f: any) {
+  return (str: string) => f.changeName.call(obj, str)
+}
 
+funcCall(One, helperObj)('BindCall')
+console.log(One.name)
+
+//3
+helperObj.setAge.call(Two, 50)
+//@ts-ignore
+console.log(Two.age)
+
+//4
+//@ts-ignore
+One.hiCall = function () {
+  helperObj.greeting.call(Two)
+}
+//@ts-ignore
+One.hiCall()
 
 // just a plug
-export default () => {};
+export default () => {}
